80229ace91ea9091999db514cd408208a1cd0484
Danilo Spinella <oss@danyspin97.org>
2021-09-12 20:12:52 +0200

    install_target: Improve destination handling
    When installing a directory, we don't want to append its name
    to the actual destination on the filesystem.
    
    I.e. when
    
    data:
      - src: mydir
    
    $ ls mydir
    file
    
    We want to have /usr/share/mypkg/file1 as opposed to
    /usr/share/mypkg/mydir/file1.

--- src/install_target.rs
+++ src/install_target.rs
@@ -15,29 +15,31 @@ impl InstallTarget {
         install_dir: &Path,
         parent_dir: &Path,
     ) -> Result<Self> {
-        let destination = install_dir.join(if let Some(destination) = entry.destination {
-            ensure!(
-                destination.is_relative(),
-                "the destination part of a file must be relative"
-            );
-            destination
-        } else {
-            PathBuf::from(
-                entry
-                    .source
-                    .file_name()
-                    .with_context(|| format!("unable to get file name from {:?}", entry.source))?,
-            )
-        });
+        let full_source = parent_dir.join(&entry.source);
+        let destination =
+            if full_source.is_file() || entry.destination.is_some() {
+                install_dir.join(if let Some(destination) = entry.destination {
+                    ensure!(
+                        destination.is_relative(),
+                        "the destination part of a file must be relative"
+                    );
+                    destination
+                } else {
+                    PathBuf::from(entry.source.file_name().with_context(|| {
+                        format!("unable to get file name from {:?}", entry.source)
+                    })?)
+                })
+            } else {
+                install_dir.to_path_buf()
+            };
         ensure!(
             entry.source.is_relative(),
             "the source file {:?} is not relative",
             entry.source
         );
-        let source = parent_dir.join(entry.source);
 
         Ok(Self {
-            source,
+            source: full_source,
             destination,
             templating: entry.templating,
         })
