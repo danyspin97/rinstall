c2de5cc327e37d76e91a69f0ce47c20d6b5e97e7
Danilo Spinella <oss@danyspin97.org>
2021-11-07 18:12:28 +0100

    package_info: Initial commit

---
+++ src/package_info.rs
@@ -0,0 +1,93 @@
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+use color_eyre::eyre::{ensure, Context, Result};
+use serde::{Deserialize, Serialize};
+
+use crate::{
+    dirs::Dirs,
+    utils::{append_destdir, write_to_file},
+};
+
+#[derive(Serialize, Deserialize)]
+pub struct InstalledFile {
+    pub path: PathBuf,
+    pub checksum: String,
+    pub replace: bool,
+}
+
+impl InstalledFile {
+    pub fn has_been_modified(&self) -> Result<bool> {
+        Ok(self.checksum
+            != blake3::hash(
+                &fs::read(&self.path)
+                    .with_context(|| format!("unable to read file {:?}", self.path))?,
+            )
+            .to_hex()
+            .to_string())
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PackageInfo {
+    #[serde(skip)]
+    pub pkg_name: String,
+    pub path: PathBuf,
+    pub files: Vec<InstalledFile>,
+}
+
+impl PackageInfo {
+    pub fn new(
+        pkg_name: &str,
+        dirs: &Dirs,
+    ) -> Self {
+        Self {
+            pkg_name: pkg_name.to_string(),
+            path: dirs
+                .localstatedir
+                .join("rinstall")
+                .join(format!("{}.pkg", &pkg_name)),
+            files: Vec::new(),
+        }
+    }
+
+    pub fn add_file(
+        &mut self,
+        path: &Path,
+        installed_path: &Path,
+        replace: bool,
+    ) -> Result<()> {
+        let file = InstalledFile {
+            path: Path::new("/").join(installed_path),
+            checksum: blake3::hash(
+                &fs::read(path).with_context(|| format!("unable to read file {:?}", path))?,
+            )
+            .to_hex()
+            .to_string(),
+            replace,
+        };
+
+        self.files.push(file);
+
+        Ok(())
+    }
+
+    pub fn install(
+        &self,
+        destdir: Option<&str>,
+    ) -> Result<()> {
+        let path = append_destdir(&self.path, &destdir);
+        fs::create_dir_all(path.parent().unwrap())
+            .with_context(|| format!("unable to create parent directory for {:?}", path))?;
+        write_to_file(
+            &path,
+            &serde_yaml::to_string(self)
+                .with_context(|| format!("unable to serialize installation into {:?}", path))?,
+        )
+        .with_context(|| format!("unable to write installation info in {:?}", path))?;
+
+        Ok(())
+    }
+}
