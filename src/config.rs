e26c6f66bd86a4d749a6607ea53331436f64fe71
Danilo Spinella <danilo.spinella@suse.com>
2022-01-21 17:25:00 +0100

    config: Restrict some directories to system-wide only

--- src/config.rs
+++ src/config.rs
@@ -60,11 +60,11 @@ pub struct Config {
     pub exec_prefix: Option<String>,
     #[clap(long)]
     pub bindir: Option<String>,
-    #[clap(long)]
+    #[clap(long, requires = "system")]
     pub sbindir: Option<String>,
     #[clap(long)]
     pub libdir: Option<String>,
-    #[clap(long)]
+    #[clap(long, requires = "system")]
     pub libexecdir: Option<String>,
     #[clap(long)]
     pub datarootdir: Option<String>,
@@ -76,13 +76,13 @@ pub struct Config {
     pub localstatedir: Option<String>,
     #[clap(long)]
     pub runstatedir: Option<String>,
-    #[clap(long)]
+    #[clap(long, requires = "system")]
     pub includedir: Option<String>,
-    #[clap(long)]
+    #[clap(long, requires = "system")]
     pub docdir: Option<String>,
-    #[clap(long)]
+    #[clap(long, requires = "system")]
     pub mandir: Option<String>,
-    #[clap(long)]
+    #[clap(long, requires = "system")]
     pub pam_modulesdir: Option<String>,
     #[clap(long)]
     pub systemd_unitsdir: Option<String>,
